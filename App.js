664d7f339b10f76e7c1142db0ee5ceeebf9b101b
warinthon30 <jaitrong3016@gmail.com>
2020-09-17 09:50:07 +0700

    commit2

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <Text onPress={doIt}>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
