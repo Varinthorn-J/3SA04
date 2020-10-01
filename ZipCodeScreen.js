3a5ebe7f190f2d20c1f7cddc176ed99a398e9a5c
warinthon30 <jaitrong3016@gmail.com>
2020-10-01 13:58:02 +0700

    commit 9

--- ZipCodeScreen.js
+++ ZipCodeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { ImageBackground, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { View, Text, FlatList,TouchableHighlight } from 'react-native';
+import { View, Text, FlatList, TouchableHighlight } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
+    { place: 'Trang', code: '92120' },
     { place: 'Chiangmai', code: '50000' },
     { place: 'Khonkaen', code: '40000' },
     { place: 'Chonburi', code: '20000' },
@@ -14,8 +15,8 @@ const availableZipItems = [
 const ZipItem = ({ place, code, navigation }) => (
     <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code })}>
         <View>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+            <Text style={styles.backdrop}>{place}</Text>
+            <Text style={styles.code}>{code}</Text>
         </View>
     </TouchableHighlight>
 )
@@ -27,13 +28,42 @@ export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
         <View>
-            <FlatList
-                data={availableZipItems}
-                keyExtractor={_keyExtractor}
-                renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
-            />
-            <StatusBar style="auto" />
+            <ImageBackground source={require('./assets/sky.jpg')} style={styles.back}>
+                <FlatList
+                    data={availableZipItems}
+                    keyExtractor={_keyExtractor}
+                    renderItem={({ item }) => <ZipItem {...item} navigation={navigation} />}
+                />
+                <StatusBar style="auto" />
+
+            </ImageBackground>
         </View>
     );
 
 }
+
+const styles = StyleSheet.create({
+    backdrop: {
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        fontSize: 35,
+
+        color: 'black',
+
+
+    },
+
+    back: {
+
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+
+    },
+    code: {
+        fontSize: 20,
+        flex:0.7,
+        backgroundColor: 'white',
+
+    },
+});
